bddbd9422d9d01bc847fc96a749d40522c9f7c45
李亚国 <1075543143@qq.com>
2018-11-23 09:24:38 +0800

    选择排序

---
+++ select_sort.py
@@ -0,0 +1,11 @@
+def select(li):
+    for i in range(len(li)-1):
+        min_loc = i
+        for j in range(i+1,len(li)):
+            if li[j] < li[min_loc]:
+                min_loc =j
+        li[i],li[min_loc] = li[min_loc],li[i]
+
+li = [3,4,2,7,8,5,6,9,1]
+select(li)
+print(li)
